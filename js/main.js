fede6bc8e64f37e5a268b9ff665b6db73e2fe44b
Multiv3rse <a.subscriptions@stecker.us>
2022-07-01 18:22:58 +0200

    commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var menuList = document.getElementById("menuList");
+
+menuList.style.maxHeight = "0px";
+
+function togglemenu() {
+    if (menuList.style.maxHeight == "0px") {
+        menuList.style.maxHeight = "6rem";
+    }
+    else {
+        menuList.style.maxHeight = "0px";
+    }
+}
